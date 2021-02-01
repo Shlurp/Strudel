d051be1706ed99cd17ad8b0dbf202b60352eb883
Shlurp <No>
2021-02-01 09:51:37 +0200

    Fixed small issue with demo

--- test.asm
+++ test.asm
@@ -4,7 +4,7 @@ MOV RBP RSP     ; Not necessary for the entrypoint - I'll explain later
 PUSH 100        ; Pushes 100 onto the stack
 CALL fun        ; Calls the "subroutine" fun
 PUSH 999        ; Pushes 999 onto the stack
-MOV RSP 0       ; The value located at the stack pointer is made 0
+MOV QWORD [RSP] 0 ; The value located at the stack pointer is made 0
 END             ; End the program
 
 TAG fun         ; Declares a tag, in this case, it's used as a subroutine
@@ -14,4 +14,4 @@ PUSH 200        ; Pushes 200 onto the stack
 LEA RSP [RSP - 8] ; Moves RSP back 8
 POP RBP     ; Gets the frame pointer
 POP RIP     ; The instruction pointer was pushed onto the stack by CALL, and now this returns to the calling func
-; Run: $dum -sir ./test.asm
+; Run: $dum -sir test.asm
